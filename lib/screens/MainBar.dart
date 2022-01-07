a1aeb4411fba6283359dabb4864b596364417be6
Basma Seyam <basmaseyam1991@gmail.com>
2022-01-07 12:01:27 +0200

    added mainbar.dart

---
+++ lib/screens/MainBar.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:moshtryate_new/constants.dart';
+import 'package:moshtryate_new/controller/file_controller.dart';
+import 'package:moshtryate_new/data/category.dart';
+import 'package:moshtryate_new/data/itemscat.dart';
+import 'package:moshtryate_new/models/cart.dart';
+import 'package:moshtryate_new/screens/NewCategory.dart';
+import 'package:moshtryate_new/screens/NewItem.dart';
+import 'package:moshtryate_new/screens/checkout.dart';
+import 'package:provider/provider.dart';
+import 'NewItem.dart';
+import 'checkout.dart';
+import 'package:moshtryate_new/widgets/searchbar.dart';
+
+class MainBar extends StatefulWidget {
+  MainBar(BuildContext context, Cart cart, Widget child);
+
+  @override
+  _MainBarState createState() => _MainBarState();
+}
+
+class _MainBarState extends State<MainBar> {
+  @override
+  Widget build(BuildContext contex) {
+    var dropdownvalue = 'item';
+
+    return AppBar(
+      titleSpacing: 0,
+      title: Text(
+        "مشترياتي",
+        textDirection: TextDirection.rtl,
+        style: TextStyle(
+          fontFamily: 'Vibes',
+          fontSize: 35,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      actions: [
+        Row(children: [
+          // a row for + and cart icon at appbar
+          DropdownButton(
+            underline: Container(color: Colors.transparent),
+            icon: Icon(
+              Icons.add,
+              size: 32,
+            ),
+            iconEnabledColor: Colors.white,
+            iconSize: 32,
+            value: 'item',
+            items: <String>['item', 'category']
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                  alignment: AlignmentDirectional.centerEnd,
+                  value: value,
+                  child: value == 'item'
+                      ? Text(
+                          'إضافة منتج',
+                          style: TextStyle(color: kMainColor),
+                          textAlign: TextAlign.right,
+                        )
+                      : Text(
+                          'إضافة قائمة',
+                          style: TextStyle(color: kMainColor),
+                          textAlign: TextAlign.right,
+                        ),
+                  onTap: () {
+                    value == 'item'
+                        ? Navigator.of(context).push(
+                            MaterialPageRoute(builder: (context) => NewItem()))
+                        : Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => NewCategory()));
+                  });
+            }).toList(),
+            onChanged: (String newValue) {
+              setState(() {
+                dropdownvalue = newValue;
+                print(dropdownvalue);
+              });
+            },
+          ),
+
+          Padding(
+            padding: EdgeInsets.only(left: 32),
+            child: Row(
+              children: [
+                IconButton(
+                  icon: Image(
+                    image:
+                        AssetImage('images/icons/icons8-shopping-cart-60.png'),
+                  ),
+                  iconSize: 30,
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => CheckoutPage()));
+                  },
+                ),
+                Text(
+                  cart.count.toString(),
+                ),
+              ],
+            ),
+          ),
+        ])
+      ],
+      bottom: PreferredSize(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(12, 0, 12, 11),
+          child: TextField(
+            autofocus: false,
+            decoration: InputDecoration(
+              contentPadding:
+                  const EdgeInsets.symmetric(vertical: 12.0, horizontal: 10),
+              //  prefixIcon: Icon(Icons.search),    // to add search icon before text
+              filled: true,
+              border: OutlineInputBorder(),
+              hintText: 'البحث عن منتج',
+              fillColor: Colors.white,
+              focusColor: Colors.white,
+            ),
+            onTap: () {
+              showSearch(
+                context: context,
+                delegate: Searchbar(),
+                query: '',
+              );
+            },
+          ),
+        ),
+        preferredSize: Size(0, 70.0),
+      ),
+    );
+  }
+}
--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,12 +1,9 @@
 import 'dart:io';
-import 'package:flat_icons_flutter/flat_icons_flutter.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:moshtryate_new/constants.dart';
 import 'package:moshtryate_new/controller/file_controller.dart';
 import 'package:moshtryate_new/data/category.dart';
 import 'package:moshtryate_new/data/itemscat.dart';
@@ -15,7 +12,6 @@ import 'package:moshtryate_new/models/cart.dart';
 import 'package:moshtryate_new/models/category.dart';
 import 'package:moshtryate_new/models/item.dart';
 import 'package:moshtryate_new/screens/NewCategory.dart';
-import 'package:moshtryate_new/widgets/searchbar.dart';
 import 'package:provider/provider.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:moshtryate_new/ad_helper.dart';
@@ -25,6 +21,7 @@ import 'package:moshtryate_new/ad_helper.dart';
 import 'NewItem.dart';
 import 'checkout.dart';
 import 'drawer.dart';
+import 'MainBar.dart';
 
 class HomePage extends StatefulWidget {
   static String id = 'HomePage';
@@ -47,10 +44,10 @@ class _HomePageState extends State<HomePage> {
   var dropdownvalue = 'item';
   @override
   void initState() {
-    if (FileManager().readJsonFile() == null) {
+    if (FileManager().readJsonFile() != null) {
       context.read<FileController>().writeCart();
     }
-    if (FileManager().readCategoryFile() == null &&
+    if (FileManager().readCategoryFile() != null &&
         FileManager().readJsonFile() != null) {
       context.read<FileController>().writeCategory();
     }
@@ -110,112 +107,9 @@ class _HomePageState extends State<HomePage> {
           home: Directionality(
             textDirection: TextDirection.rtl,
             child: Scaffold(
-              appBar: AppBar(
-                titleSpacing: 0,
-                title: Text(
-                  "مشترياتي",
-                  textDirection: TextDirection.rtl,
-                  style: TextStyle(
-                    fontFamily: 'Vibes',
-                    fontSize: 35,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                actions: [
-                  Row(children: [
-                    // a row for + and cart icon at appbar
-                    DropdownButton(
-                      underline: Container(color: Colors.transparent),
-                      icon: Icon(
-                        Icons.add,
-                        size: 32,
-                      ),
-                      iconEnabledColor: Colors.white,
-                      iconSize: 32,
-                      value: 'item',
-                      onChanged: (String newValue) {
-                        setState(() {
-                          dropdownvalue = newValue;
-                        });
-                      },
-                      items: <String>['item', 'category']
-                          .map<DropdownMenuItem<String>>((String value) {
-                        return DropdownMenuItem<String>(
-                            alignment: AlignmentDirectional.centerEnd,
-                            value: value,
-                            child: value == 'item'
-                                ? Text(
-                                    'إضافة منتج',
-                                    style: TextStyle(color: kMainColor),
-                                    textAlign: TextAlign.right,
-                                  )
-                                : Text(
-                                    'إضافة قائمة',
-                                    style: TextStyle(color: kMainColor),
-                                    textAlign: TextAlign.right,
-                                  ),
-                            onTap: () {
-                              value == 'item'
-                                  ? Navigator.of(context).push(
-                                      MaterialPageRoute(
-                                          builder: (context) => NewItem()))
-                                  : Navigator.of(context).push(
-                                      MaterialPageRoute(
-                                          builder: (context) => NewCategory()));
-                            });
-                      }).toList(),
-                    ),
-
-                    Padding(
-                      padding: EdgeInsets.only(left: 32),
-                      child: Row(
-                        children: [
-                          IconButton(
-                            icon: Image(
-                              image: AssetImage(
-                                  'images/icons/icons8-shopping-cart-60.png'),
-                            ),
-                            iconSize: 35,
-                            onPressed: () {
-                              Navigator.of(context).push(MaterialPageRoute(
-                                  builder: (context) => CheckoutPage()));
-                            },
-                          ),
-                          Text(
-                            cart.count.toString(),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ])
-                ],
-                bottom: PreferredSize(
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(12, 0, 12, 11),
-                    child: TextField(
-                      autofocus: false,
-                      decoration: InputDecoration(
-                        contentPadding: const EdgeInsets.symmetric(
-                            vertical: 12.0, horizontal: 10),
-                        //  prefixIcon: Icon(Icons.search),    // to add search icon before text
-                        filled: true,
-                        border: OutlineInputBorder(),
-                        hintText: 'البحث عن منتج',
-                        fillColor: Colors.white,
-                        focusColor: Colors.white,
-                      ),
-                      onTap: () {
-                        showSearch(
-                          context: context,
-                          delegate: Searchbar(),
-                          query: '',
-                        );
-                      },
-                    ),
-                  ),
-                  preferredSize: Size(0, 70.0),
-                ),
-              ),
+              appBar: PreferredSize(
+                  child: MainBar(context, cart, child),
+                  preferredSize: Size.fromHeight(120)),
               drawer: MyDrawer(),
               body: FutureBuilder<void>(
                   future: _initGoogleMobileAds(),
