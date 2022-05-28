3a7fa40d4e397e69a827717838ff2e3daba9dfe2
AyaAbouebeid <aya.abouebeid@gmail.com>
2022-05-28 12:06:13 +0200

    fixed issue outdated android version

--- android/app/src/main/kotlin/com/example/moshtryate_new/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/moshtryate_new/MainActivity.kt
@@ -1,6 +1,6 @@
-package com.example.moshtryate_new
+package com.example.moshtryate_new;
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterActivity;
 
 class MainActivity: FlutterActivity() {
 }
